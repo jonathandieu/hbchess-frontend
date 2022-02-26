e0351de9facbe7fcbc6dd30ab62aaae53213756a
Abraham Hernandez <abeherham@knights.ucf.edu>
2022-02-26 14:44:05 -0500

    Installed and set up tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing that Automatic Deployment Works!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1 className="text-5xl text-center text-green-500 font-serif">
+        Welcome! <br />
+        Everything is fine
+      </h1>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom'; // Used to keep UI in sync with the URL, only reloading components that need to be changed instead of entire page
 ReactDOM.render(
   <React.StrictMode>
@@ -12,8 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
