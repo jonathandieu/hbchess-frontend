4e58bc35608dd40c846063bf0dadc897e7f284c0
Abraham Hernandez <abeherham@knights.ucf.edu>
2022-03-03 20:52:19 -0500

    Fix routing and style bug

--- src/components/LoggedOutTemplate.tsx
+++ src/components/LoggedOutTemplate.tsx
@@ -5,24 +5,27 @@ const LoggedOutTemplate: React.FC = ({ children }) => {
   return (
     <div className="grid grid-cols-5 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12 h-screen">
       <div className="col-span-1 bg-cadet pl-2 pt-4 flex flex-col space-y-10">
-        <Link to="/" className="text-2xl text-gray-50 hover:text-nyanza">
+        <Link
+          to="/"
+          className="text-lg xl:text-xl 2xl:text-2xl text-gray-50 hover:text-nyanza"
+        >
           BHChess
         </Link>
         <Link
-          to="leaderboard"
-          className="text-2xl text-gray-50 hover:text-nyanza"
+          to="/leaderboard"
+          className="xl:text-xl 2xl:text-2xl text-gray-50 hover:text-nyanza"
         >
           Leaderboard
         </Link>
         <Link
           to="/auth/signup"
-          className="text-2xl text-gray-50 hover:text-nyanza"
+          className="xl:text-xl 2xl:text-2xl text-gray-50 hover:text-nyanza"
         >
           Sign Up
         </Link>
         <Link
           to="/auth/login"
-          className="text-2xl text-gray-50 hover:text-nyanza"
+          className="xl:text-xl 2xl:text-2xl text-gray-50 hover:text-nyanza"
         >
           Log In
         </Link>
