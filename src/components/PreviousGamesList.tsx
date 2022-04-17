1ec85f9bf32d3a11510d8b62a5cea6007f5d6b56
Jara <araj_mont@yahoo.com>
2022-04-17 00:02:16 -0400

    Added the following components to the dashboard:
    - Play a game button
    - Skeleton for list of previous games

---
+++ src/components/PreviousGamesList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PreviousGamesList = () => {
+  return (
+    <>
+      <h1 className="pb-3 w-full text-3xl border-b-2 border-gray-300">
+        Previous Games:
+      </h1>
+      <div className="flex gap-x-48 pb-1 mt-5 text-xl border-b-2 border-gray-300">
+        <div>Your Team</div>
+        <div>Opponent Team</div>
+        <div>Result</div>
+      </div>
+    </>
+  );
+};
+
+export default PreviousGamesList;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
+import PlayGameButton from '../components/PlayGameButton';
+import PreviousGamesList from '../components/PreviousGamesList';
 function Dashboard() {
   return (
     <div className="flex justify-center items-center w-full h-full">
-      <h1 className="font-serif text-5xl text-center text-green-500">
-        Welcome! <br />
-        Everything is Dashboard
-      </h1>
+      <div className="flex flex-col items-center p-8 w-full text-gray-100 bg-gray-800 rounded-lg shadow-2xl xl:w-2/3 2xl:w-2/3">
+        <PlayGameButton toLink="/dashboard/play">Play A Game</PlayGameButton>
+        <PreviousGamesList />
+      </div>
     </div>
   );
 }
